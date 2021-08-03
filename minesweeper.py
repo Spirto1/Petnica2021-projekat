ea58e039942320927325c381912230b765d778a0
Spirto1 <irfospirto@gmail.com>
2021-08-03 17:58:14 +0200

    playGame() pomeren na bota

--- minesweeper.py
+++ minesweeper.py
@@ -97,47 +97,4 @@ class boardClass(object):
     def isWinner(self):
         return self.selectableSpots == 0
 
-numWin = 0
-numLoss = 0
-numPlay = 0
-#play game
-def playGame(boardSize, numMines):
-    global numWin, numLoss
-    #boardSize = int(input("Choose the Width of the board: "))
-    #numMines = int(input("Choose the number of mines: "))
-    gameOver = False
-    winner = False
-    Board = boardClass(boardSize, numMines)
-    while not gameOver:
-        #print(Board)
-        x = int(input("X: "))
-        y = int(input("Y: "))
-        Board.makeMove(x, y)
-        gameOver = Board.hitMine(x, y)
-        if Board.isWinner() and gameOver == False:
-            gameOver = True
-            winner = True
 
-    print(Board)
-    if winner:
-        print("Congratulations, You Win!")
-        numWin += 1
-        endGame(numWin, numLoss)
-    else:
-        print("You hit a mine, Game Over!")
-        numLoss += 1
-        endGame(numWin, numLoss)
-        
-def endGame(numWin, numLoss):
-    global numPlay
-    f = open("result.txt", "w")
-    f.write("\nWins: " + str(numWin))
-    f.write("\nLosses: " + str(numLoss))
-    f.write("\nTotal: " + str(numWin + numLoss))
-    f.close()
-    if numPlay != 10:
-        playGame()
-        numPlay += 1
-
-       
-playGame()
